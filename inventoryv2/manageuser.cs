2d6adae3bb86f3dfd79b13717a6388dc49a82fe4
ThanujiW <thanuji.wijerathna@gmail.com>
2023-07-10 11:04:36 -0700

    doen user add

--- inventoryv2/manageuser.cs
+++ inventoryv2/manageuser.cs
@@ -18,13 +18,49 @@ namespace inventoryv2
         {
             InitializeComponent();
         }
-
+        private DataTable userDataTable;
+        private string[,] userDataArray;
+        string fileName = "E:\\inventory2\\inventoryv2\\user.txt";
         private void manageuser_Load(object sender, EventArgs e)
         {
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
 
+                userDataArray = new string[lines.Length, 4];
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] values = lines[i].Split(',');
+                    userDataArray[i, 0] = values[0];
+                    userDataArray[i, 1] = values[1];
+                    userDataArray[i, 2] = values[2];
+                    userDataArray[i, 3] = values[3];
+                }
+
+                userDataTable = new DataTable();
+
+                
+                userDataTable.Columns.Add("Username");
+                userDataTable.Columns.Add("Full Name");
+                userDataTable.Columns.Add("Password");
+                userDataTable.Columns.Add("Telephone");
+
+                int rows = userDataArray.GetLength(0);
+                for (int i = 0; i < rows; i++)
+                {
+                    DataRow row = userDataTable.NewRow();
+                    row["Username"] = userDataArray[i, 0];
+                    row["Full Name"] = userDataArray[i, 1];
+                    row["Password"] = userDataArray[i, 2];
+                    row["Telephone"] = userDataArray[i, 3];
+                    userDataTable.Rows.Add(row);
+                }
+                bunifuCustomDataGrid1.DataSource = userDataTable;
+            }
         }
-        private DataTable userDataTable;
-        private string[] userDataArray;
+        
+
         private void button1_Click(object sender, EventArgs e)
         {
             string username = userName.Text;
@@ -32,27 +68,23 @@ namespace inventoryv2
             string password = bunifuMaterialTextbox3.Text;
             string telephone = bunifuMaterialTextbox4.Text;
 
-            // Create an array to store the input values
             if (userDataTable == null)
             {
-                userDataTable = new DataTable();
-                userDataTable.Columns.Add("Username");
+                userDataTable = new DataTable("Users");
+                userDataTable.Columns.Add(new DataColumn("Username"));
                 userDataTable.Columns.Add("Full Name");
                 userDataTable.Columns.Add("Password");
                 userDataTable.Columns.Add("Telephone");
             }
 
-            // Optionally, display a message or perform other actions with the stored data
             DataRow newRow = userDataTable.NewRow();
             newRow["Username"] = username;
             newRow["Full Name"] = fullName;
             newRow["Password"] = password;
             newRow["Telephone"] = telephone;
             userDataTable.Rows.Add(newRow);
-            MessageBox.Show("Data stored successfully!");
 
-            // Save the data to a txt file
-            string fileName = "data.txt";
+            string fileName = "E:\\inventory2\\inventoryv2\\user.txt";
             StreamWriter sw = File.CreateText(fileName);
             foreach (DataRow row in userDataTable.Rows)
             {
@@ -60,7 +92,6 @@ namespace inventoryv2
             }
             sw.Close();
 
-            // Bind the DataTable to the DataGridView
             bunifuCustomDataGrid1.DataSource = userDataTable;
         }
 
